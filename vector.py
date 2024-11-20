06f1b7b593e2b89786c47cfefd4df81e29bbe03f
Aniruddha Mukherjee <aniruddha.mukherji@outlook.com>
2024-11-21 02:28:51 +0530

    add: vector

---
+++ vector.py
@@ -0,0 +1,48 @@
+class Vector:
+    def __init__(self, data=[0.0]) -> None:
+        self.data = data
+        self.dim = len(data)
+
+    def __getitem__(self, key):
+        return self.data[key]
+
+    def __setitem__(self, key, val):
+        if key < 0 or key >= self.dim:
+            raise IndexError(f"key must be be between 0 and {self.dim - 1}")
+        self.data[key] = val
+
+    def __add__(self, rhs):
+        new_vec = Vector([0.0] * self.dim)
+        if isinstance(rhs, (float, int)):
+            new_vec.data = [x + rhs for x in self.data]
+        elif isinstance(rhs, Vector):
+            new_vec.data = [x + y for (x, y) in zip(self.data, rhs.data)]
+        return new_vec
+
+    def __sub__(self, rhs):
+        new_vec = Vector([0.0] * self.dim)
+        if isinstance(rhs, (float, int)):
+            new_vec.data = [x - rhs for x in self.data]
+        elif isinstance(rhs, Vector):
+            new_vec.data = [x - y for (x, y) in zip(self.data, rhs.data)]
+        return new_vec
+
+    def __mul__(self, rhs):
+        new_vec = Vector([0.0] * self.dim)
+        if isinstance(rhs, (float, int)):
+            new_vec.data = [x * rhs for x in self.data]
+        elif isinstance(rhs, Vector):
+            new_vec.data = [x * y for (x, y) in zip(self.data, rhs.data)]
+        return new_vec
+
+    def dot(self, rhs):
+        if not isinstance(rhs, Vector):
+            raise ValueError("Can only dot with a vector")
+        if rhs.dim != self.dim:
+            raise ValueError(
+                f"Expected {self.dim}-dimensional vector, received {rhs.dim}-dimensional vector instead"
+            )
+        return sum((self * rhs).data)
+
+    def __str__(self) -> str:
+        return f"{self.dim}-dimensional Vector at {hex(id(self))}: {self.data}"
